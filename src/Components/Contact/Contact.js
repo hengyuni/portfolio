436ff603fa80f39a9e904a09f86a7cfca73aaed1
hengyuni <hengyuni@gmail.com>
2021-08-09 14:53:05 -0400

    contacts

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,35 +1,41 @@
-import React from 'react'
-import * as contactStyle from './Contact.module.css'
+import React from "react";
+import * as contactStyle from "./Contact.module.css";
 
 const Contact = () => {
-  return (
-    <div className={contactStyle.contactContainer}>
-      <form className={contactStyle.form}>
-        {/* <h2 className={contactStyle.h2}>Contact form</h2> */}
+	return (
+		<div className={contactStyle.contacContainer}>
+			<b>Email:</b>{" "}
+			<a className={contactStyle.a} href="mailto: hengyuni@gmail.com">
+				hengyuni@gmail.com
+			</a>
+		</div>
+		// <div className={contactStyle.contactContainer}>
+		//   <form className={contactStyle.form}>
+		//     {/* <h2 className={contactStyle.h2}>Contact form</h2> */}
 
-        <label className={contactStyle.label}>Name</label>
-        <input 
-          placeholder="Name"
-          className={contactStyle.input}
-        />
+		//     <label className={contactStyle.label}>Name</label>
+		//     <input
+		//       placeholder="Name"
+		//       className={contactStyle.input}
+		//     />
 
-        <label className={contactStyle.label}>Email</label>
-        <input 
-          placeholder="Email"
-          className={contactStyle.input}
-        />
+		//     <label className={contactStyle.label}>Email</label>
+		//     <input
+		//       placeholder="Email"
+		//       className={contactStyle.input}
+		//     />
 
-        <label className={contactStyle.label}>Message</label>
-        <textarea 
-          placeholder="Message"
-          className={contactStyle.textarea}
-        />
+		//     <label className={contactStyle.label}>Message</label>
+		//     <textarea
+		//       placeholder="Message"
+		//       className={contactStyle.textarea}
+		//     />
 
-        <button type="submit" className={contactStyle.button}>Submit</button>
+		//     <button type="submit" className={contactStyle.button}>Submit</button>
 
-      </form>
-    </div>
-  )
-}
+		//   </form>
+		// </div>
+	);
+};
 
-export default Contact
+export default Contact;
