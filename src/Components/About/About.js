f35012096c1241a62eaaea1b13d476d11fc3d50e
hengyuni <hengyuni@gmail.com>
2021-08-09 14:56:26 -0400

    changed the photo

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,13 +2,12 @@ import React from "react";
 import * as aboutStyle from "./About.module.css";
 
 function About() {
+	const imgOne = "https://i.imgur.com/T4kI92O.jpg";
+	const imgTwo = "https://i.imgur.com/gHTdog7.jpg";
+
 	return (
 		<div className={aboutStyle.aboutContainer}>
-			<img
-				src="https://i.imgur.com/T4kI92O.jpg"
-				alt="headshot"
-				className={aboutStyle.headerImg}
-			/>
+			<img src={imgTwo} alt="headshot" className={aboutStyle.headerImg} />
 			<p className={aboutStyle.body}>
 				MERN-stack developer with background in traditional sign painting and
 				television production. A lover of music, reading, skateboarding, and the
